4e1c4386360b512f54934032a97276f65febbbbd
Andrew Matheus <andrewmatheus@gmail.com>
2021-03-18 00:00:02 -0300

    Commit challenge chapter II
    - Desafio separação de components;

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{ 
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  dataMovies: MovieProps[];
+}
+
+export function Content({ dataMovies }: ContentProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {dataMovies.map(movie => (
+          <MovieCard 
+            key={movie.Title}
+            title={movie.Title} 
+            poster={movie.Poster} 
+            runtime={movie.Runtime} 
+            rating={movie.Ratings[0].Value} 
+          />
+        ))}
+      </div>
+    </main>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  onPressButton: (id: number) => void;
+}
+
+export function SideBar({ selectedGenreId, genres, onPressButton }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={genre.id}
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onPressButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
